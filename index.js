d82e8f9a81be8016156cdaab760015dbec907db2
Sasonda <sasonda0902@gmail.com>
2025-07-20 11:10:10 +0700

    <fix>[Add Upload Image and Document]

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import 'dotenv/config';
 import express from 'express';
 import multer from "multer";
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
@@ -30,14 +31,13 @@ const run = async () => {
 
 
 
-
+// prompt text
 app.get('/', (req, res) => {
   res.send('Hello World! test')
 })
 app.get('/about', (req, res) => {
   res.send('about page')
 })
-
 app.post('/generate-text', async (req, res) => {
   const prompt = 
   req.body?.prompt || "Introduce yourself as a friendly AI assistant.";
@@ -51,6 +51,59 @@ app.post('/generate-text', async (req, res) => {
     res.status(500).json({error: error.message});   
   }
 });
+
+
+// image
+const imageToGenerativePart = filePath => {
+  const imageData = fs.readFileSync(filePath);
+  return {
+    inlineData: {
+      mimeType: 'image/jpeg/png/jpg',
+      data: Buffer.from(imageData).toString('base64'),
+    },
+  };
+};
+app.post('/generate-image', upload.single('image'), async (req, res) => {
+  const prompt = req.body?.prompt || "Describe the image";
+
+  try {
+    const image = imageToGenerativePart(req.file?.path);
+    const result = await model.generateContent([prompt, image]);
+    res.status(200).json({
+      output: result.response.text(),
+      // image: result.response.image,
+    });
+  } catch (error) {
+    res.status(500).json({error: error.message});   
+  }
+});
+
+
+// dokument
+app.post('/generate-document', upload.single('document'), async (req, res) => {
+  const prompt = req.body?.prompt || "Rangkum dokumen ini";
+  const filePath = req.file?.path;
+  const buffer = fs.readFileSync(filePath);
+  const base64Data = buffer.toString('base64');
+  const mimeType = req.file?.mimetype;
+
+  try {
+    const documentPart = {
+      inlineData: {
+        mimeType: mimeType,
+        data: base64Data,
+      },
+    };
+    const result = await model.generateContent([prompt, documentPart]);
+    res.status(200).json({
+      output: result.response.text(),
+    });
+  } catch (error) {
+    res.status(500).json({error: error.message});   
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
